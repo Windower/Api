a8162a12187442e000183be1d1b902640a9437d7
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2024-10-21 12:08:15 +0200

    Refactored for future hook/core support

--- PluginsUpdateHandler.cs
+++ PluginsUpdateHandler.cs
@@ -2,49 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Linq;
-using PeNet;
 
 namespace Windower.Api;
 
-public class PluginsUpdateHandler : UpdateHandler {
-	private String DevPath { get; set; } = null!;
-	private String ManifestPath { get; set; } = null!;
-	private String PluginsPath { get; set; } = null!;
-	private XDocument Manifest { get; set; } = null!;
-	private XElement PluginsNode { get; set; } = null!;
-	private Boolean Changed { get; set; }
-	private IDictionary<String, Plugin> Plugins { get; set; } = new Dictionary<String, Plugin>();
+public class PluginsUpdateHandler : VersionedUpdateHandler {
+	protected override String RelativeTargetPath { get; } = "plugins";
 
-	public override async Task Initialize(Config config) {
-		DevPath = Path.Combine(config.FilesPath, "4", "dev");
-		ManifestPath = Path.Combine(DevPath, "manifest.xml");
+	private String PluginsPath { get; set; } = null!;
+	private XElement PluginsNode { get; set; } = null!;
+	private IDictionary<String, Item> Plugins { get; set; } = new Dictionary<String, Item>();
+
+	protected override void Initialize() {
 		PluginsPath = Path.Combine(DevPath, "plugins");
-		Manifest = await ReadManifest();
 		PluginsNode = Manifest.Root!.Element("plugins")!;
 		Plugins = PluginsNode.Elements("plugin").ToDictionary(
 			element => element.Element("name")!.Value.ToLowerInvariant(),
-			Plugin.Parse
+			Item.Parse
 		);
 	}
 
-	public override async Task CheckVersion(String filename, Stream stream) {
-		using var memoryStream = new MemoryStream();
-		await stream.CopyToAsync(memoryStream);
-		var buffer = memoryStream.ToArray();
-		var peFile = new PeFile(buffer);
-		var version = Version.Parse(peFile.Resources!.VsVersionInfo!.StringFileInfo.StringTable[0].FileVersion!.Replace(", ", "."));
-
-		var name = filename[..^4];
+	protected override Item GetItem(String name) {
 		var lower = name.ToLowerInvariant();
-		if (!Plugins.TryGetValue(lower, out var plugin)) {
+		if (!Plugins.TryGetValue(lower, out var item)) {
 			var element = new XElement("plugin", new XElement("name", name), new XElement("version", new Version(0, 0, 0, 0)));
-			plugin = Plugin.Parse(element);
-			Plugins[lower] = plugin;
+			item = Item.Parse(element);
+			Plugins[lower] = item;
 
 			Plugins
 				.OrderBy(kvp => kvp.Key)
@@ -52,47 +35,6 @@ public class PluginsUpdateHandler : UpdateHandler {
 				.Value.Element.AddAfterSelf(element);
 		}
 
-		if (version < plugin.Version) {
-			throw new Exception($"{name} version {version} less than previous version {plugin.Version}.");
-		}
-
-		if (version == plugin.Version) {
-			return;
-		}
-
-		using var file = File.Open(Path.Combine(PluginsPath, plugin.Name), FileMode.Create, FileAccess.Write, FileShare.None);
-		using var ms = new MemoryStream(buffer);
-		await ms.CopyToAsync(file);
-
-		plugin.Element.SetElementValue("version", version);
-
-		Changed = true;
-	}
-
-	public override async Task Finalize(Config config) {
-		if (!Changed) {
-			return;
-		}
-
-		using var file = File.Open(ManifestPath, FileMode.Create, FileAccess.Write, FileShare.None);
-		using var writer = XmlWriter.Create(file, new() {
-			Async = true,
-			Encoding = new UTF8Encoding(false),
-			OmitXmlDeclaration = true,
-			Indent = true,
-			NewLineChars = "\n",
-			NewLineHandling = NewLineHandling.Entitize,
-		});
-		await Manifest.SaveAsync(writer, CancellationToken.None);
-	}
-
-	private async Task<XDocument> ReadManifest() {
-		using var stream = File.Open(ManifestPath, FileMode.Open, FileAccess.Read, FileShare.Read);
-		return await XDocument.LoadAsync(stream, LoadOptions.None, CancellationToken.None);
-	}
-
-	private record struct Plugin(String Name, XElement Element, Version Version) {
-		public static Plugin Parse(XElement element) =>
-			new(element.Element("name")!.Value, element, Version.Parse(element.Element("version")!.Value));
+		return item;
 	}
 }
---
+++ VersionedUpdateHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using PeNet;
+
+namespace Windower.Api;
+
+public abstract class VersionedUpdateHandler : UpdateHandler {
+	protected String DevPath { get; set; } = null!;
+	protected String ManifestPath { get; set; } = null!;
+	protected String TargetPath { get; set; } = null!;
+	protected XDocument Manifest { get; set; } = null!;
+	protected Boolean Changed { get; set; }
+
+	protected abstract String RelativeTargetPath { get; }
+
+	public sealed override async Task Initialize(Config config) {
+		DevPath = Path.Combine(config.FilesPath, "4", "dev");
+		ManifestPath = Path.Combine(DevPath, "manifest.xml");
+		Manifest = await ReadManifest();
+		TargetPath = Path.Combine(DevPath, RelativeTargetPath);
+
+		Initialize();
+	}
+
+	private async Task<XDocument> ReadManifest() {
+		using var stream = File.Open(ManifestPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+		return await XDocument.LoadAsync(stream, LoadOptions.None, CancellationToken.None);
+	}
+
+	public sealed override async Task CheckVersion(String filename, Stream stream) {
+		using var memoryStream = new MemoryStream();
+		await stream.CopyToAsync(memoryStream);
+		var buffer = memoryStream.ToArray();
+		var peFile = new PeFile(buffer);
+		var version = Version.Parse(peFile.Resources!.VsVersionInfo!.StringFileInfo.StringTable[0].FileVersion!.Replace(", ", "."));
+		var name = filename[..^4];
+
+		var item = GetItem(name);
+
+		if (version < item.Version) {
+			throw new Exception($"{name} version {version} less than previous version {item.Version}.");
+		}
+
+		if (version == item.Version) {
+			return;
+		}
+
+		using var file = File.Open(Path.Combine(TargetPath, item.Name ?? name), FileMode.Create, FileAccess.Write, FileShare.None);
+		using var ms = new MemoryStream(buffer);
+		await ms.CopyToAsync(file);
+
+		item.Element.SetElementValue("version", version);
+
+		Changed = true;
+	}
+
+	public sealed override async Task Finalize(Config config) {
+		if (!Changed) {
+			return;
+		}
+
+		using var file = File.Open(ManifestPath, FileMode.Create, FileAccess.Write, FileShare.None);
+		using var writer = XmlWriter.Create(file, new() {
+			Async = true,
+			Encoding = new UTF8Encoding(false),
+			OmitXmlDeclaration = true,
+			Indent = true,
+			NewLineChars = "\n",
+			NewLineHandling = NewLineHandling.Entitize,
+		});
+		await Manifest.SaveAsync(writer, CancellationToken.None);
+	}
+
+	protected abstract void Initialize();
+	protected abstract Item GetItem(String name);
+
+	protected record struct Item(String? Name, XElement Element, Version Version) {
+		public static Item Parse(XElement element) =>
+			new(element.Element("name")?.Value, element, Version.Parse(element.Element("version")!.Value));
+	}
+}
