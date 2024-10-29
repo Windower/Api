e0023fff76e4a8d68389e73982a5c30088063637
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2024-10-29 05:31:36 +0100

    Fixed spelling

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ var app = builder.Build();
 var mutex = new AsyncLock();
 var rest = new HttpClient();
 
-app.MapGet("/api/ping", () => "Pong");
+app.MapGet("/api/ping", () => "pong");
 
 app.MapPost("/api/gh", async (HttpRequest request) => {
 	request.HttpContext.Features.Get<IHttpMaxRequestBodySizeFeature>()!.MaxRequestBodySize = 100 * 1024 * 1024;
