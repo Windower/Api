d0710aebec1a1773f88703bb1dfa1b235f633108
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2024-11-02 15:05:26 +0100

    Action/Upload: Added output

--- .github/actions/upload/upload.js
+++ .github/actions/upload/upload.js
@@ -12,10 +12,14 @@ try {
 	const form = new FormData();
 	form.append("api-key", apiKey);
 	form.append("repository", repository);
+	core.setOutput(`Packing files in ${directory}:`);
 	for (const relative of (await fs.readdir(directory)).filter(relative => !extension || relative.endsWith(`.${extension}`))) {
+		core.setOutput(`  Reading ${relative}`);
 		form.append(relative, new Blob([await fs.readFile(path.join(directory, relative))]));
 	}
+	core.setOutput("Finished packing files.");
 
+	core.setOutput(`Calling ${url}...`);
 	const response = await fetch(url, { method: "POST", body: form });
 
 	const chunks = [];
@@ -25,7 +29,7 @@ try {
 	const message = Buffer.concat(chunks).toString("utf8");
 
 	const fn = (response.status === 200 ? core.info : core.setFailed);
-	fn(`${response.statusText} (${response.status})${(message !== "" ? `: ${message}` : "")}`);
+	fn(`  ${response.statusText} (${response.status})${(message !== "" ? `: ${message}` : "")}`);
 } catch (error) {
 	core.setFailed(`Error during processing: ${error}`);
 }
