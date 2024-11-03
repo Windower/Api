ba0c0d4599dfc944d703a40ef4f760413b1d0d5a
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2024-11-03 04:08:46 +0100

    Refactored raw update handler out of website handler

---
+++ RawUpdateHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Windower.Api;
+
+public abstract class RawUpdateHandler : UpdateHandler {
+	private String RootPath { get; set; } = null!;
+
+	public override Task Initialize(Config config) {
+		RootPath = GetRoot(config);
+		return Task.CompletedTask;
+	}
+
+	public override async Task CheckVersion(String filename, MemoryStream stream) =>
+		await SaveFile(stream, Path.Combine(RootPath, filename));
+
+	public override Task Finalize() =>
+		Task.CompletedTask;
+
+	protected abstract String GetRoot(Config config);
+}
--- WebsiteUpdateHandler.cs
+++ WebsiteUpdateHandler.cs
@@ -1,23 +1,8 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace Windower.Api;
 
-public class WebsiteUpdateHandler : UpdateHandler {
-	private String RootPath { get; set; } = null!;
-
-	public override Task Initialize(Config config) {
-		RootPath = config.WebsitePath;
-		return Task.CompletedTask;
-	}
-
-	public override async Task CheckVersion(String filename, MemoryStream stream) {
-		var path = Path.Combine(RootPath, filename);
-
-		await SaveFile(stream, path);
-	}
-
-	public override Task Finalize() =>
-		Task.CompletedTask;
+public class WebsiteUpdateHandler : RawUpdateHandler {
+	protected override String GetRoot(Config config) =>
+		config.WebsitePath;
 }
